757f33dd461c38932cd8bb3fab55637230a1ce8a
abhiperfecct <prajapatiabhishek4321@gmail.com>
2025-04-23 15:34:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const PORT = 3000;
 
 
 const corsOptions = {
-    origin: ['http://localhost:5173' ,'https://localhost:5173'],
+    origin: [process.env.CLIENT_URL_1,process.env.CLIENT_URL_2],
     optionsSuccessStatus: 200 
 }
 
-//app.use(cors(corsOptions)); 
-app.use(cors());
+app.use(cors(corsOptions)); 
+//app.use(cors());
 app.use(express.json()); 
 
 app.get('/', ( req,  res) =>{
